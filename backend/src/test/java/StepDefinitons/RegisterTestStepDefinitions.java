57d1dbb05343f8b78d1ba1627192798ccea3c24a
Krisztian Toth <tothkrisztian1569@gmail.com>
2022-04-28 18:51:03 +0200

    Feat: Registration tests

--- backend/src/test/java/StepDefinitons/RegisterTestStepDefinitions.java
+++ backend/src/test/java/StepDefinitons/RegisterTestStepDefinitions.java
@@ -1,31 +1,48 @@
 package StepDefinitons;
 
+import Pages.LoginAndRegisterPage;
+import Pages.ProfilePage;
+import Util.DriverUtil;
+import Util.Urls;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.jupiter.api.Assertions;
 
 public class RegisterTestStepDefinitions {
-    @Given("the user is on the registration page")
+    protected static DriverUtil driverUtil;
+    private final LoginAndRegisterPage loginAndRegisterPage = new LoginAndRegisterPage(driverUtil.getDriver());
+    private final ProfilePage profilePage = new ProfilePage(driverUtil.getDriver());
+
+    @Given("The user is on the registration page")
     public void theUserIsOnTheRegistrationPage() {
+        driverUtil.getDriver().get(Urls.REGISTRATION_PAGE.url);
     }
 
     @And("Enter the password fill with {string}")
-    public void enterThePasswordFillWith(String arg0) {
+    public void enterThePasswordFillWith(String password) {
+        loginAndRegisterPage.inputPassword(password);
     }
 
-    @Then("See on top-right {string}")
-    public void seeOnTopRight(String arg0) {
+    @Then("See on top-right Profile")
+    public void seeOnTopRightProfile() {
+        Assertions.assertTrue(loginAndRegisterPage.hasProfileButton());
     }
 
-    @And("Click to {string}")
+    @And("Click to Profile")
     public void clickToProfile() {
+        loginAndRegisterPage.clickProfileButton();
     }
 
     @Then("Validate user email, expected: {string}")
-    public void validateUserEmailExpected(String arg0) {
+    public void validateUserEmailExpected(String email) {
+        Assertions.assertEquals(profilePage.getEmailFromProfile(), email);
     }
 
-    @Then("The user sees top-right {string} button")
+    @Then("The user sees top-right login button")
     public void theUserSeesTopRightLoginButton() {
+        Assertions.assertTrue(loginAndRegisterPage.hasLoginButton());
     }
 }
--- backend/src/test/resources/Registration-Tests.feature
+++ backend/src/test/resources/Registration-Tests.feature
@@ -2,25 +2,24 @@ Feature: Registration Tests
 
     Scenario: Empty credentials Registration
         
-        Given the user is on the registration page
-        When do not write to email and password field
-        And click to 'Submit' button
-        Then the user sees top-right 'login' button
+        Given The user is on the registration page
+        When Click to Submit button
+        Then the user sees top-right login button
 
     Scenario: Successful registration
 
         Given The user is on the registration page
-        When Enter the e-mail field with "automationTest1@gmail.com"
-        And Enter the password fill with "abc123"
-        And Click to 'Submit' button
-        Then See on top-right "Profile"
-        And Click to 'Profile'
-        Then Validate user email, expected: "automationTest1@gmail.com"
+        When Enter the e-mail field with "automationTest12734589@gmail.com"
+        And Enter the password fill with "abc1323"
+        And Click to Submit button
+        Then See on top-right Profile
+        And Click to Profile
+        Then Validate user email, expected: "automationTest12734589@gmail.com"
 
+    @last
     Scenario: Empty password Registration
 
         Given The user is on the registration page
-        When Enter the e-mail field with "automationTest1@gmail.com"
-        And Do not write to password field!
-        And And Click to 'Submit' button
-        Then The user sees top-right 'login' button
+        When Enter the e-mail field with "automationTest23447589@gmail.com"
+        And Click to Submit button
+        Then The user sees top-right login button
